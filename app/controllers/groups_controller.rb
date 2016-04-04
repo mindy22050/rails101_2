399342593680afa965450efae7de89537b3c90a2
mindy <mindy22050@gmail.com>
2016-04-04 15:50:05 +0800

    可以瀏覽單一討論版

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,5 +7,26 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-  
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+
 end
