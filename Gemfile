d7eeed0acd7c1154bbd0d2f4fe93b285748f2aaa
mindy <mindy22050@gmail.com>
2016-04-04 14:34:50 +0800

    可以新增討論版 ( 建立 action new, create )

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-
+#手動製作CRUD架構
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def notice_message
+    alert_types = { notice: :success, alert: :danger }
+    close_botten_options = {class:"close", "data-dismiss" =>"alert" ,"aria-hidden" => true}
+    close_botten = content_tag(:bottom, 'x',close_botten_options)
+
+    alerts = flash.map do |type, message|
+      alert_content =close_botten + message
+
+      alert_type = alert_types[type.to_sym] ||type
+      alert_class = "alert alert-#{alert_type} alert-dismissable"
+
+      content_tag(:div, alert_content,class: alert_class)
+    end
+
+    alerts.join("\n").html_safe
+  end
 end
