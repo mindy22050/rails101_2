da442b175858a870a7c985832263c04f53623cde
mindy <mindy22050@gmail.com>
2016-04-04 16:06:46 +0800

    資料驗證: title 不能空白

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,6 +36,12 @@ class GroupsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: '已刪除你的廢文'
+  end
+
   private
 
   def group_params
