9a2280801e70e1f5308ed97adeda4fb08c7d6dc6
mindy <mindy22050@gmail.com>
2016-04-04 15:59:28 +0800

    修改討論版與儲存已改的設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,20 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: '你的廢文已經改好惹'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def group_params
